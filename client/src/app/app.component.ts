e300debe55bdd59aef46be151fecdcca8e4d7d05
pge7181 <pge7181@gmail.com>
2024-11-18 15:59:47 -0500

    End of section 3

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,17 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HttpClientModule, HttpClient } from '@angular/common/http'; // Ensure HttpClientModule is imported
+import { CommonModule } from '@angular/common'; // Import CommonModule
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ RouterOutlet, HttpClientModule ], // Add HttpClientModule here
+  imports: [ RouterOutlet, HttpClientModule, CommonModule ], // Add HttpClientModule here
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ] // Corrected typo: styleUrl -> styleUrls
 })
 export class AppComponent implements OnInit {
   title = 'The Dating App';
-  users: any;
+  users: any[] = [];
 
   constructor(private http: HttpClient) { }
 
@@ -20,10 +21,14 @@ export class AppComponent implements OnInit {
   }
 
   getUsers() {
-    this.http.get('http://localhost:5267/api/users').subscribe(response => {
-      this.users = response;
-    }, error => {
-      console.log(error);
-    });
+    this.http.get('http://localhost:5267/api/users').subscribe(
+      (response: any) => {
+        console.log(response);
+        this.users = response;
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
   }
 }
